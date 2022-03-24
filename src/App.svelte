7c769b84d5664b5856121d5c26c2ead2ca7bcff7
Sebastian Gug <seb@owlee.software>
2022-03-24 08:45:58 +0000

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  const random = "random";
+  import Subscriber from "./lib/subscriber.svelte";
+</script>
+
+<div style="display: flex; width: 100%; height: 100%; justify-content: center;">
+  <Subscriber channel="one" />
+</div>
---
+++ src/lib/subscriber.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+  import { io } from "socket.io-client";
+  import type { Socket } from "socket.io-client";
+  import { onMount } from "svelte";
+
+  export let channel: string;
+
+  let socket: Socket;
+  let open: boolean = false;
+  let err: any;
+  let viewers = 0;
+  let donations = 0;
+
+  onMount(() => {
+    socket = io("http://localhost:3000", {
+      host: "http://localhost",
+      port: 3000,
+      rememberUpgrade: true,
+      upgrade: true,
+      extraHeaders: {
+        channel,
+      },
+    });
+
+    socket.io.on("open", () => {
+      open = true;
+      err = undefined;
+    });
+
+    socket.io.on("close", () => {
+      open = false;
+    });
+
+    socket.io.on("error", (error) => {
+      err = error as any;
+    });
+
+    socket.on("VIEWERS", (data) => {
+      console.log("EVENT EMITTED", "VIEWERS");
+
+      console.log(data);
+    });
+
+    socket.connect();
+  });
+</script>
+
+<div
+  style="border: 5px solid grey; padding: 0.5rem; display: flex; flex-direction: column; justify-content: center; align-items: space-between;"
+>
+  <div
+    style="display: flex; flex-direction: row; justify-content: center; align-items: center;"
+  >
+    <p>Channel: <b> {channel} </b></p>
+    <div
+      style="width:50px; height: 50px; margin: 20px;"
+      style:background={open ? "green" : "red"}
+    />
+  </div>
+
+  <div>
+    <p>Viewers: {viewers}</p>
+    <p>Donations: {donations}</p>
+  </div>
+
+  {#if err}
+    <div style="color: red;">
+      <pre>{err}</pre>
+    </div>
+  {/if}
+</div>
